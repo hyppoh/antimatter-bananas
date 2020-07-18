b8e32224eccfda14161bd6d8c58fad69a8a8e65f
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-07-18 12:44:49 +0800

    added back probability

--- js/core/salvaging.js
+++ js/core/salvaging.js
@@ -2,7 +2,7 @@ function salvage() {
 	if (player.bananas.lt(1)) return;
 	player.bananas = player.bananas.sub(1);
 	player.antimatter = player.antimatter.add(0.00001);
-	player.seeds = player.seeds.add(0.5);
+	player.seeds = player.seeds.add(Math.round(Math.random()*2));
 }
 function plant() {
 	if (player.seeds.lt(1)) return;
@@ -12,13 +12,13 @@ function plant() {
 function destroy() {
 	if (player.trees.lt(1)) return;
 	player.trees = player.trees.sub(1);
-	player.bananas = player.bananas.add(3);
+	player.bananas = player.bananas.add((Math.round(Math.random()*5));
 }
 function salvageDozen() {
 	if (player.bananas.lt(12)) return;
 	player.bananas = player.bananas.sub(12);
 	player.antimatter = player.antimatter.add(0.00012);
-	player.seeds = player.seeds.add(6);
+	player.seeds = player.seeds.add((Math.round(Math.random()*12)+6);
 }
 setInterval(() => {
 	if (player.pendingseeds.gte(1)) {
