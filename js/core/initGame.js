9888596ca9180583331ad1de5ac3ad69ba07c337
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-08-21 13:43:40 +0800

    fucking el

--- js/core/initGame.js
+++ js/core/initGame.js
@@ -1,17 +1,17 @@
-let initPlayer = {
+var initPlayer = {
 	bananas: new Decimal(6),
 	antimatter: new Decimal(0),
 	seeds: new Decimal(0),
 	growing: new Decimal(0),
 	trees: new Decimal(0),
 	money: new Decimal(0),
-	freespace: new Decimal(1)
+	freespace: new Decimal(1),
 	lastTick: performance.now()
 };
-let player = initPlayer;
-let time = 30;
-let m = 15;
-let el = {
+var player = initPlayer;
+var time = 30;
+var m = 15;
+var el = {
 	bananaamt: document.getElementById("bananaamt"),
 	amamt: document.getElementById("antimatteramt"),	
 	seedamt: document.getElementById("seedamt"),
