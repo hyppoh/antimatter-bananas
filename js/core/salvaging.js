68e5865e33860922ed75c88f551668126d5fb1b6
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-08-21 13:24:17 +0800

    Revert "v1.04, money and space and whatnot"

--- js/core/salvaging.js
+++ js/core/salvaging.js
@@ -7,13 +7,12 @@ function salvage() {
 function plant() {
 	if (player.seeds.lt(1)) return;
 	player.seeds = player.seeds.sub(1);
-	player.growingseeds = player.growingseeds.add(1);
+	player.pendingseeds = player.pendingseeds.add(1);
 }
 function destroy() {
 	if (player.trees.lt(1)) return;
 	player.trees = player.trees.sub(1);
 	player.bananas = player.bananas.add(Math.round(Math.random()*5));
-	player.seeds = player.seeds.add(Math.round(Math.random()*2))
 }
 function salvageDozen() {
 	if (player.bananas.lt(12)) return;
@@ -21,15 +20,10 @@ function salvageDozen() {
 	player.antimatter = player.antimatter.add(0.00012);
 	player.seeds = player.seeds.add((Math.round(Math.random()*12)+6));
 }
-function sellDozen() {
-	if (player.bananas.gte(144)) return;
-	player.bananas = player.bananas.sub(12)
-	player.money = player.money.add(Math.round(Math.random()*7)+2)
-}
 setInterval(() => {
-	if (player.growingseeds.gte(1)) {
-		var tosub = player.growingseeds.pow(0.5).ceil().max(8).min(player.growingseeds);
-		player.growingseeds = player.growingseeds.sub(tosub);
+	if (player.pendingseeds.gte(1)) {
+		var tosub = player.pendingseeds.pow(0.5).ceil().max(8).min(player.pendingseeds);
+		player.pendingseeds = player.pendingseeds.sub(tosub);
 		player.trees = player.trees.add(tosub);
 	}
 }, m*1000);
