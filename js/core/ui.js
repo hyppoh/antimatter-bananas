00c45eb630c97c078f0e3f7a0087938d96a677c8
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-08-22 10:38:05 +0800

    order

--- js/core/ui.js
+++ js/core/ui.js
@@ -7,9 +7,9 @@ function toSci(num, places, placesafter1000) {
 function updateUI() {
 	el.bananaamt.innerText = `you have ${toSci(player.bananas)} bananas`;
 	el.seedamt.innerText = `you have ${toSci(player.seeds.floor())} banana seeds`;
+	el.growingamt.innerText = `you have ${toSci(player.growing)} seeds waiting to grow into trees`;
 	el.amamt.innerText = `you have ${toSci(player.antimatter, 5)} antimatter`;
 	el.treeamt.innerText = `you have ${toSci(player.trees)} banana trees to produce bananas for you`;
-	el.growingamt.innerText = `you have ${toSci(player.growing)} banana seeds waiting to grow into trees`;
 	el.moneyamt.innerText = `you have ${toSci(player.money)} money to spend`;
 	el.freespaceamt.innerText = `you have ${toSci(player.freespace)} free space to use for planting`;
 	if (performance.now()-player.lastTick > m*1000) {
