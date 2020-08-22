dc752e9c9ffdaed427cec7fb390839da03f8f721
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-08-22 10:22:32 +0800

    seeds -> banana seeds

--- js/core/ui.js
+++ js/core/ui.js
@@ -9,7 +9,7 @@ function updateUI() {
 	el.seedamt.innerText = `you have ${toSci(player.seeds.floor())} banana seeds`;
 	el.amamt.innerText = `you have ${toSci(player.antimatter, 5)} antimatter`;
 	el.treeamt.innerText = `you have ${toSci(player.trees)} banana trees to produce bananas for you`;
-	el.growingamt.innerText = `you have ${toSci(player.growing)} seeds waiting to grow into trees`;
+	el.growingamt.innerText = `you have ${toSci(player.growing)} banana seeds waiting to grow into trees`;
 	el.moneyamt.innerText = `you have ${toSci(player.money)} money to spend`;
 	el.freespaceamt.innerText = `you have ${toSci(player.freespace)} free space to use for planting`;
 	if (performance.now()-player.lastTick > m*1000) {
