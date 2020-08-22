1012e771012af96b1dda3b4c1ddab064fa8ebd3f
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-08-22 10:11:40 +0800

    v1.04

--- js/core/ui.js
+++ js/core/ui.js
@@ -8,11 +8,13 @@ function updateUI() {
 	el.bananaamt.innerText = `you have ${toSci(player.bananas)} bananas`;
 	el.seedamt.innerText = `you have ${toSci(player.seeds.floor())} banana seeds`;
 	el.amamt.innerText = `you have ${toSci(player.antimatter, 5)} antimatter`;
-	el.treeamt.innerText = `you have ${toSci(player.trees)} banana trees`;
-	el.pendingamt.innerText = `you have ${toSci(player.pendingseeds)} seeds waiting to grow into trees`;
+	el.treeamt.innerText = `you have ${toSci(player.trees)} banana trees to produce bananas for you`;
+	el.growingamt.innerText = `you have ${toSci(player.growing)} seeds waiting to grow into trees`;
+	el.moneyamt.innerText = `you have ${toSci(player.money)} money to spend`;
+	el.freespaceamt.innerText = `you have ${toSci(player.freespace)} free space to use for planting`;
 	if (performance.now()-player.lastTick > m*1000) {
 		player.bananas = player.bananas.add(player.trees);
 		player.lastTick = performance.now();
 	}
 }
-setInterval(updateUI, 50);
+setInterval(updateUI, 33);
