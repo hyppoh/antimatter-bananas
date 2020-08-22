36e7ca7b34a651c5620aaf226d31de99862fb8e7
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-08-22 10:10:39 +0800

    v1.04

--- js/core/initGame.js
+++ js/core/initGame.js
@@ -2,17 +2,21 @@ let initPlayer = {
 	bananas: new Decimal(6),
 	antimatter: new Decimal(0),
 	seeds: new Decimal(0),
-	pendingseeds: new Decimal(0),
+	growing: new Decimal(0),
 	trees: new Decimal(0),
+	money: new Decimal(0),
+	freespace: new Decimal(1)
 	lastTick: performance.now()
 };
 let player = initPlayer;
-let time = 25;
+let time = 30;
 let m = 15;
-let el = {
+var el = {
 	bananaamt: document.getElementById("bananaamt"),
-	amamt: document.getElementById("antimatteramt"),	
+	amamt: document.getElementById("antimatteramt"),
 	seedamt: document.getElementById("seedamt"),
 	treeamt: document.getElementById("treeamt"),
-	pendingamt: document.getElementById("pendingamt")
+	growingamt: document.getElementById("growingamt"),
+	moneyamt: document.getElementById("moneyamt"),
+	freespaceamt: document.getElementById("freespaceamt")
 };
