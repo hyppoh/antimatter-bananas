6552d05d34468da4493cafe2461eeb1bc8fc0368
Hyppoh <68418800+hyppoh@users.noreply.github.com>
2020-08-23 17:31:16 +0800

    banana -> bananas

--- js/core/ui.js
+++ js/core/ui.js
@@ -5,7 +5,7 @@ function toSci(num, places, placesafter1000) {
 	return (num.e < 3) ? (num.m*Math.pow(10, num.e)).toFixed(places) : `${num.m.toFixed(placesafter1000)}e${num.e}`
 }
 function updateUI() {
-	el.bananaamt.innerText = `you have ${toSci(player.bananas)} bananas`;
+	el.bananasamt.innerText = `you have ${toSci(player.bananas)} bananas`;
 	el.seedamt.innerText = `you have ${toSci(player.seeds.floor())} banana seeds`;
 	el.growingamt.innerText = `you have ${toSci(player.growing)} seeds waiting to grow into trees`;
 	el.amamt.innerText = `you have ${toSci(player.antimatter, 5)} antimatter`;
